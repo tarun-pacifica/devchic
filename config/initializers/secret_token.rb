51f1ca1f2da8d5e744b6a8bd3acc7208cee7cefb
Stephen McGillen <smcgillen@hotmail.com>
2013-07-19 18:10:27 +1000

    devchic

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Devchic::Application.config.secret_token = '30dba3372b33df3fdf2e0255b43d8a34f3b7311e29a326b1ae9fdbbc057765aca53dba5cbef228c17abc59282b00de87fbd3030c31de5e72a3777532824a8975'
